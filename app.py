46ac427dedd38528fcb74dc8da54bc9fe144fff5
Sam <89320104+voidsf@users.noreply.github.com>
2025-06-10 17:27:57 +0000

    extract methods related to sentiment analysis from jyoti.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def init_app():
+    st.set_page_config(layout="wide")
+    st.title("Event Unfolder: A Real-Time Story Tracker ⏳")
+
+    st.sidebar.header("Filter by entities")
+    
+
