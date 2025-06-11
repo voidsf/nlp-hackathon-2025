1b4052b0dfbd4794257b027ace8a8a7f865ee05b
GooeyFlyer <garrettt3@cardiff.ac.uk>
2025-06-11 11:25:02 +0100

    testing

--- latest_code.py
+++ latest_code.py
@@ -56,5 +56,10 @@ def get_common_keywords_df(df):
 
 # Example usage
 
+data = {"id": [1,2,3],
+        "title": ["Xbox game pass", "Xbox pass", "game time"]}
+
+df = pd.DataFrame(data)
+
 common_df = get_common_keywords_df(df)
 print(common_df)
